540913c37d9e6355c87495c7b1fbc764ce7ee082
rohitbarvekar <rohitbarvekar97@gmail.com>
2024-12-24 00:08:43 +0530

    Product service: small config change

--- ProductService/src/main/java/dev/rohit/productservice/config/RedisConfig.java
+++ ProductService/src/main/java/dev/rohit/productservice/config/RedisConfig.java
@@ -1,22 +1,32 @@
 package dev.rohit.productservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
-        @Bean
-        public JedisConnectionFactory jedisConnectionFactory() {
-            return new JedisConnectionFactory();
-        }
 
-        @Bean
-        public RedisTemplate<String, Object> redisTemplate() {
-            RedisTemplate<String, Object> template = new RedisTemplate<>();
-            template.setConnectionFactory(jedisConnectionFactory());
-            return template;
-        }
+    @Value("${redis.host}")
+    private String redisHost;
+
+    @Value("${redis.port}")
+    private int redisPort;
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
 
 }
